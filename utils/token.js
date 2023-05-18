ee91295e59d768cb01f21af5082bf7afc4f016e5
Doan Hoa (段梨花) <q81188118@gmail.com>
2023-05-18 20:03:28 +0800

    Update token.js

--- utils/token.js
+++ utils/token.js
@@ -1,4 +1,5 @@
 import { verify } from 'jsonwebtoken';
+import { sign } from 'jsonwebtoken';
 
 export const verifyToken = (token, secretKey) => {
   try {
@@ -9,3 +10,11 @@ export const verifyToken = (token, secretKey) => {
     return null;
   }
 };
+
+
+export const generateAccessToken = (user) => {
+  const secretKey = process.env.SECRET_KEY; // Replace with your actual secret key
+  const token = sign({ sub: user.id }, secretKey, { expiresIn: '1h' }); // Customize the expiration as needed
+  return token;
+};
+
