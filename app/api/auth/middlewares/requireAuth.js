4072b54a8610e9a1c3c239039568e5fb804b7089
Doan Hoa (段梨花) <q81188118@gmail.com>
2023-05-18 20:12:36 +0800

    Update requireAuth.js
    1

--- app/api/auth/middlewares/requireAuth.js
+++ app/api/auth/middlewares/requireAuth.js
@@ -1,18 +1,20 @@
 import { verify } from 'jsonwebtoken';
-import { getToken } from "next-auth/jwt";
-import jwt from "jsonwebtoken";
+import { Response } from 'next';
+
 export const requireAuth = (handler) => {
   return async (req) => {
     try {
-      const token = await getToken({
-        req: req,
-        secret: secret,
-        raw: true,
-      });
+      const token = req.headers.authorization?.split(' ')[1];
+
+      if (!token) {
+        return new Response('Unauthorized', { status: 401 });
+      }
+
+      const secretKey = process.env.SECRET_KEY; // Replace with your actual secret key
 
       // Verify the access token using your secret key
-      const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
-      console.log(`decodedToken`, decodedToken);
+      const decodedToken = verify(token, secretKey);
+      console.log('decodedToken', decodedToken);
 
       if (!decodedToken) {
         return new Response('Unauthorized', { status: 401 });
@@ -21,8 +23,8 @@ export const requireAuth = (handler) => {
       req.user = decodedToken.user; // Assuming the user object is present in the token
       return handler(req);
     } catch (err) {
-      console.error("Auth error:", err);
-      return new Response("Access denied" , { status: 500 });
+      console.error('Auth error:', err);
+      return new Response('Access denied', { status: 500 });
     }
   };
 };
