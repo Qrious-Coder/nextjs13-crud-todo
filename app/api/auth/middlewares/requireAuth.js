379ccde78ebccacd461a13982ef08950cc8d55b8
Doan Hoa (段梨花) <q81188118@gmail.com>
2023-05-20 19:32:04 +0800

    Update requireAuth.js
    a

--- app/api/auth/middlewares/requireAuth.js
+++ app/api/auth/middlewares/requireAuth.js
@@ -3,15 +3,15 @@ import { verify } from 'jsonwebtoken';
 const secretKey = process.env.SECRET_KEY; // Replace with your actual secret key
 
 export const requireAuth = (handler) => {
-  return async (req) => {
+  return async (req) => {let
     try {
       //Todo: save token to cookies
       // Or learn to decode next-auth-session-cookies
       const authorizationHeader = 
-        req.headers instanceof headers
+        req.headers instanceof Headers
           ? req.headers.get('authorization')
           : req.headers.authorization
-      const token = authorizationHeader?.split(' ')[1];
+      let token = authorizationHeader?.split(' ')[1];
 
       //BUG: can't get token from headers
       // req.headers is an Object which cannot extract [authorization]
@@ -19,22 +19,22 @@ export const requireAuth = (handler) => {
 
       console.log('token', token);
       if (!token) {
-        return res.status(401).json('No token found!');
+        return new Response( 'No token found!', { status: 401 } )
       }
 
       // Verify the access token using your secret key
       const decodedToken = verify(token, secretKey);
-      console.log('decodedToken', decodedToken);
+      // console.log('decodedToken', decodedToken);
 
       if (!decodedToken) {
-        return res.status(401).json('Cannot decoded token');
+        return new Response( 'Cannot decode!', { status: 401 } )
       }
 
       req.user = decodedToken.sub; // Assuming the user object is present in the token
       return handler(req);
     } catch (err) {
       console.error('Auth error:', err);
-      return res.status(500).json('Auth error');
+      return  return new Response( `Mw server error: ${err}`, { status: 500 } )
     }
   };
 };
