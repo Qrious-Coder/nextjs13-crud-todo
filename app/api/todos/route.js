a3a47cf96500125f6d12f684952974b3794605a8
Doan Hoa (段梨花) <q81188118@gmail.com>
2023-05-20 18:00:36 +0800

    Update route.js with Filter/pagination/sorting
    Update route.js with Filter/pagination/sorting function

--- app/api/todos/route.js
+++ app/api/todos/route.js
@@ -4,10 +4,43 @@ import { requireAuth } from "@/app/api/auth/middlewares/requireAuth";
 export const GET = requireAuth(async (req) => {
   try {
     await dbConnect();
-    const todos = await Todo.find({ user: req.user._id });
+    const { priority, status, page, limit, sortBy } = req.query
+    const query = { user: req.user._id }
+    
+    //Filter by priorty and status
+    if(priority){
+      query.priority = priority
+    }
+    
+    if(status){
+      if( status === 'completed'){
+        query.completed = true
+      }else if(status === 'uncompleted'){
+        query.completed = false
+      }  
+    }
+    
+    //Pagination
+    const pageNumber = parseInt(page) || 1;
+    const pageSize = parseInt(limit) || 10;
+    const skip = (pageNumber - 1) * pageSize;
+
+    // Fetch todos, applied filtering and pagination
+    let todos = await Todo.find(query)
+      .skip(skip)
+      .limit(pageSize);
+
+    //Sorting
+    if (sortBy) {
+      const sortField = sortBy.substring(1);
+      const sortOrder = sortBy.charAt(0) === "-" ? -1 : 1;
+      todos = todos.sort({ [sortField]: sortOrder });
+    }
+    
     return new Response(JSON.stringify(todos), { status: 200 });
+    
   } catch (error) {
     console.error(error);
-    return new Response("Failed to fetch all users", { status: 500 });
+    return new Response(`Fetching failed: ${error}`, { status: 500 });
   }
 });
