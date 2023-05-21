545f5a341cab1f8b73bd1bbfff4370388b6f1d1c
Doan Hoa (段梨花) <q81188118@gmail.com>
2023-05-22 01:50:10 +0800

    add filter

--- components/TodoList.jsx
+++ components/TodoList.jsx
@@ -1,30 +1,50 @@
+'use client'
 import React from 'react';
 import TodoItem from './TodoItem'
+import { useState} from "react";
 
-const TodoList = ({ todos, onDelete, onEdit }) => {
+const TodoList = ({ todos, onDelete, onEdit, onFilter }) => {
+  const [filter, setFilter] = useState('');
+
+  const handleFilterChange = (e) => {
+    const selectedFilter = e.target.value;
+    setFilter(selectedFilter);
+    onFilter(selectedFilter);
+  };
   return (
-    <table className="w-full border-collapse">
-      <thead>
-      <tr className="bg-black text-white">
-        <th className="py-2 px-4">Task</th>
-        <th className="py-2 px-4">Priority</th>
-        <th className="py-2 px-4">Status</th>
-        <th className="py-2 px-4">Date</th>
-        <th className="py-2 px-4">Edit</th>
-        <th className="py-2 px-4">Delete</th>
-      </tr>
-      </thead>
-      <tbody>
-      {todos.map((todo) => (
+    <>
+      <div>
+        <label htmlFor="filter">Filter By:</label>
+        <select id="filter" value={ filter } onChange={ handleFilterChange }>
+          <option value="">All</option>
+          <option value="priority">Priority</option>
+          <option value="status">Status</option>
+        </select>
+      </div>
+      <table className="w-full border-collapse">
+        <thead>
+        <tr className="bg-black text-white">
+          <th className="py-2 px-4">Task</th>
+          <th className="py-2 px-4">Priority</th>
+          <th className="py-2 px-4">Status</th>
+          <th className="py-2 px-4">Date</th>
+          <th className="py-2 px-4">Edit</th>
+          <th className="py-2 px-4">Delete</th>
+        </tr>
+        </thead>
+        <tbody>
+        {todos.map((todo) => (
           <TodoItem
             todo = { todo }
             key = { todo._id }
             onDelete = { onDelete }
-            onEdit = { onEdit } 
+            onEdit = { onEdit }
           />
-      ))}
-      </tbody>
-    </table>
+        ))}
+        </tbody>
+      </table>
+    </>
+
   );
 };
 
