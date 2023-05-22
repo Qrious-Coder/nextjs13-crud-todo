3b88afff9fe0e41eedc415e397e311f30de438eb
Doan Hoa (段梨花) <q81188118@gmail.com>
2023-05-22 15:44:16 +0800

    Update Alert.jsx
    1

--- components/Alert.jsx
+++ components/Alert.jsx
@@ -1,7 +1,7 @@
-'use client'
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import cs from 'classnames';
+import { FaTimes, FaCheckCircle, FaExclamationTriangle, FaInfoCircle } from 'react-icons/fa';
 
 const Alert = ({ children }) => {
   const [isShow, setIsShow] = useState(true);
@@ -11,25 +11,33 @@ const Alert = ({ children }) => {
     setIsShow((prevState) => !prevState);
   };
 
-
   const renderChildEl = () => {
     return React.cloneElement(children);
+  };
+
+  const getIcon = () => {
+    switch (alertType) {
+      case 'danger':
+        return <FaExclamationTriangle />;
+      case 'success':
+        return <FaCheckCircle />;
+      case 'warning':
+        return <FaExclamationTriangle />;
+      case 'info':
+        return <FaInfoCircle />;
+      default:
+        return null;
+    }
   };
 
   return (
     <>
       {showAlert && (
-        <div
-          className={cs(
-            'alert',
-            'border-yellow-800',
-            ${alertType},
-            !isShow && 'isHide'
-          )}
-        >
+        <div className={cs('alert', `border-${alertType}`, alertType, !isShow && 'isHide')}>
+          {getIcon()}
           {children ? renderChildEl() : alertText}
-         <span className='closeBtn' onClick={handleClose}>
-            &times;
+          <span className="closeBtn" onClick={handleClose}>
+            <FaTimes />
           </span>
         </div>
       )}
